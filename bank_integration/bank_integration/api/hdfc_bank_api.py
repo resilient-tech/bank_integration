3cfe6ec9a42b9fdedf06c038b7d7efd9a7432fbf
harsh patadia <harshpatadia4114@gmail.com>
2026-03-24 15:37:54 +0530

    fix: changed xpath of incorrect otp

--- bank_integration/bank_integration/api/hdfc_bank_api.py
+++ bank_integration/bank_integration/api/hdfc_bank_api.py
@@ -277,7 +277,7 @@ class HDFCBankAPI(BankAPI):
         if submit_btn:
             self.br.execute_script("arguments[0].click();", submit_btn)
             incorrect_otp_xpath = "//span[@class='text-danger' and contains(normalize-space(),'You have entered incorrect OTP.')]"
-            invalid_otp_length_xpath = "//span[contains(normalize-space(),'Please enter the 6-digit OTP') and not(contains(@class,'d-none'))]"
+            invalid_otp_length_xpath = "//span[contains(@class,'d-flex') and not(contains(@class,'d-none')) and contains(@class,'text-danger') and contains(normalize-space(),'Please enter the 6-digit OTP')]"
 
             self.wait_until(
                 AnyEC(
